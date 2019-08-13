cf869e3d97df6a8356da4cf59ff5f21fe91a0e84
Szabó Gergő <Szabogergo1992@gmail.com>
2019-08-13 13:52:33 +0200

    pom updated for optimal java version, HashMap created but not in use yet

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,10 @@
 import java.io.IOException;
 import java.io.OutputStream;
-import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.InetSocketAddress;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
@@ -16,14 +17,21 @@ public class Main {
     }
 
     private static void init() throws IOException {
+        MyHandler handler = new MyHandler();
+        handler.setupMethods();
+
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-        server.createContext("/test", new MyHandler());
-        server.createContext("/test2", new MyHandler());
+        server.createContext("/test", handler);
+        server.createContext("/test2", handler);
         server.setExecutor(null); // creates a default executor
         server.start();
     }
 
+
     static class MyHandler implements HttpHandler {
+
+        Map<String, String> methods = new HashMap<>();
+
         @Override
         public void handle(HttpExchange t) throws IOException {
 
@@ -42,15 +50,27 @@ public class Main {
                             OutputStream os = t.getResponseBody();
                             os.write(response.getBytes());
                             os.close();
-                        } catch (IllegalAccessException e) {
-                            System.out.println(e.getMessage());;
-                        } catch (InvocationTargetException f) {
-                            f.printStackTrace();
+                        } catch (IllegalAccessException | InvocationTargetException e) {
+                            e.printStackTrace();
                         }
                     }
                 }
             }
 
         }
+
+        private void setupMethods() {
+
+            for(Method m: Routes.class.getMethods()) {
+                if(m.isAnnotationPresent(WebRoute.class)) {
+                    try {
+                        methods.put(m.getAnnotation(WebRoute.class).value(), (String) m.invoke(null));
+                    } catch (IllegalAccessException | InvocationTargetException | NullPointerException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
     }
 }
